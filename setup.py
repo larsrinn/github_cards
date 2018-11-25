b42d098241a3091669b6117280ac6c9f3fd611c4
larsrinn <lm.rinn@outlook.com>
2018-11-25 19:42:53 +0100

    is rst still there

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 with open("README.md") as readme_file:
     readme = readme_file.read()
 
-with open("HISTORY.md") as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 requirements = ["Click>=6.0", "Jinja2>=2.10", "github3.py>=1"]
@@ -43,6 +43,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/larsrinn/github_cards",
-    version="0.1.10",
+    version="0.1.11",
     zip_safe=False,
 )
