33f66d44435c43509a7168b43467d7fd8ffe3376
larsrinn <lm.rinn@outlook.com>
2018-11-25 20:30:08 +0100

    include templates in build

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/larsrinn/github_cards",
-    version="0.1.15",
+    version="0.1.16",
     zip_safe=False,
 )
