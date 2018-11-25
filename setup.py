1937fdf5dbc017b70d4fe7c8253d5ce84b456392
larsrinn <lm.rinn@outlook.com>
2018-11-25 16:04:09 +0100

    update setup.py to include dependencies, etc.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.rst") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ["Click>=6.0"]
+requirements = ["Click>=6.0", "jinja2", "github3.py"]
 
 setup_requirements = ["pytest-runner"]
 
@@ -25,16 +25,11 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
     description="Convert your GitHub issues into printable cards for your physical "
-    "Scrum board.",
+    "Scrum/Kanban board.",
     entry_points={"console_scripts": ["github_cards=github_cards.cli:main"]},
     install_requires=requirements,
     license="MIT license",
