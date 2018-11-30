877bf4e5c874dd70e942ff530451547c9a02b8eb
larsrinn <lm.rinn@outlook.com>
2018-11-30 11:28:37 +0100

    determine text color of labels based on background color

--- github_cards/rendering.py
+++ github_cards/rendering.py
@@ -1,10 +1,12 @@
 from jinja2 import Environment, PackageLoader, select_autoescape
 
+from github_cards import filters
 
 env = Environment(
     loader=PackageLoader("github_cards", "templates"),
     autoescape=select_autoescape(["html", "xml"]),
 )
+env.filters["readable_font_color"] = filters.readable_font_color
 
 
 def render_cards(**context):
